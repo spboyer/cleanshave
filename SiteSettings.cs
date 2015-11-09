4217d7c5ebe5c7dd41fdd469ed524aacdb0137bd
Boyer, Shayne P <Shayne.P.Boyer@disney.com>
2015-11-09 14:20:21 -0500

    Add site settings class

---
+++ SiteSettings.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanShave
+{
+    public class SiteSettings
+    {
+        public string DefaultAdminUsername { get; set; }
+        public string DefaultAdminPassword { get; set; }
+    }
+}
