ccaf0fd673ccdf2d4593213aec95d51f987e6ff4
Boyer, Shayne P <Shayne.P.Boyer@disney.com>
2015-11-09 14:21:23 -0500

    gulp file changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,53 @@
-/// <binding Clean='clean' />
-'use strict';
-var gulp = require("gulp"),
-  rimraf = require("rimraf"),
-  concat = require("gulp-concat"),
-  cssmin = require("gulp-cssmin"),
-  uglify = require("gulp-uglify"),
-  project = require("./project.json");
+/// <binding AfterBuild='build' Clean='clean' />
 
-var paths = {
-  webroot: "./" + project.webroot + "/"
+"use strict";
+
+var path = require('path');
+var gulp = require('gulp');
+var del = require('del');
+var eventStream = require('event-stream');
+var typescript = require('gulp-typescript');
+var inlineNg2Template = require('gulp-inline-ng2-template');
+var sourcemaps = require('gulp-sourcemaps');
+
+var project = require("./project.json");
+var webroot = "./" + project.webroot + "/";
+
+var config = {
+    libBase: 'node_modules',
+    lib: [
+        require.resolve('bootstrap/dist/css/bootstrap.css'),
+        path.dirname(require.resolve('bootstrap/dist/fonts/glyphicons-halflings-regular.woff')) + '/**',
+        require.resolve('traceur/bin/traceur-runtime.js'),
+        require.resolve('es6-module-loader/dist/es6-module-loader-sans-promises.js'),
+        require.resolve('reflect-metadata/Reflect.js'),
+        require.resolve('systemjs/dist/system.src.js'),
+        require.resolve('angular2/bundles/angular2.dev.js'),
+        require.resolve('angular2/bundles/router.dev.js'),
+        require.resolve('angular2/bundles/http.dev.js'),
+        require.resolve('jquery/dist/jquery.js'),
+        require.resolve('bootstrap/dist/js/bootstrap.js')
+    ]
 };
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
-
-gulp.task("clean:js", function(cb) {
-  rimraf(paths.concatJsDest, cb);
+gulp.task('build.lib', function () {
+    return gulp.src(config.lib, { base: config.libBase })
+        .pipe(gulp.dest(webroot + 'lib'));
 });
 
-gulp.task("clean:css", function(cb) {
-  rimraf(paths.concatCssDest, cb);
+gulp.task('build', ['build.lib'], function () {
+    var tsProject = typescript.createProject('./tsconfig.json', { typescript: require('typescript') });
+    var tsSrcInlined = gulp.src([webroot + '**/*.ts'], { base: webroot })
+        .pipe(inlineNg2Template({ base: webroot }));
+    return eventStream.merge(tsSrcInlined, gulp.src('Typings/**/*.ts'))
+        .pipe(sourcemaps.init())
+        .pipe(typescript(tsProject))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(webroot));
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
-
-gulp.task("min:js", function() {
-  gulp.src([paths.js, "!" + paths.minJs], {
-      base: "."
-    })
-    .pipe(concat(paths.concatJsDest))
-    .pipe(uglify())
-    .pipe(gulp.dest("."));
+gulp.task('clean', function () {
+    return del([webroot + 'lib']);
 });
 
-gulp.task("min:css", function() {
-  gulp.src([paths.css, "!" + paths.minCss])
-    .pipe(concat(paths.concatCssDest))
-    .pipe(cssmin())
-    .pipe(gulp.dest("."));
-});
-
-gulp.task("min", ["min:js", "min:css"]);
+gulp.task('default', ['build']);
